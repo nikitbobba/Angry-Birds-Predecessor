553a999b0d43b3179bdb26a09b9acef59402cfe8
nikitbobba <nikitsbobba@gmail.com>
2018-01-16 14:18:08 -0600

    init commit - complete game
    full game available

---
+++ Assets/Code/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 2;
+    public GameObject ExplosionPrefab;
+
+
+    void Destruct()
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+    void Boom()
+    {
+        PointEffector2D pe = gameObject.GetComponent<PointEffector2D>();
+        pe.enabled = true;
+
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.enabled = false;
+
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+        Invoke("Destruct", 0.1f);
+    }
+
+    internal void OnCollisionEnter2D(Collision2D x)
+    {
+        if (x.relativeVelocity.magnitude >= ThresholdForce)
+        {
+            Boom();
+        }
+    }
+    
+    
+    
+}
---
+++ Assets/Code/TargetBox.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+    private void Scored()
+    {
+        // FILL ME IN
+        SpriteRenderer render = gameObject.GetComponent<SpriteRenderer>();
+        float mass = gameObject.GetComponent<Rigidbody2D>().mass;
+        if (render.color != Color.green)
+        {
+            ScoreKeeper.AddToScore(mass);
+            render.color = Color.green;
+        }
+    }
+
+
+    internal void OnCollisionEnter2D(Collision2D x)
+    {
+        if (x.gameObject.tag == "Ground")
+        {
+            Scored();
+        }
+    }
+}
